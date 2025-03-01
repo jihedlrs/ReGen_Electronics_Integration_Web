675aaee44366c8de76a399a19a30593d3091ba59
[Jihed Lourassi] <[jihedlrs@gmail.com]>
2025-03-01 22:56:05 +0100

    ux

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#20c997',
+        success: '#28a745',
+      }
+    }
+  },
+  plugins: [],
+};
